32e62eaa0a3c3ee5bc61ebc860b12f63263a0088
ForgetAndRemember <1965584294@qq.com>
2018-12-11 18:44:25 +0800

    添加库

---
+++ StaticLibTool.h
@@ -0,0 +1,13 @@
+//
+//  StaticLibTool.h
+//  StaticLibTool
+//
+//  Created by wangxu的air on 2018/12/10.
+//  Copyright © 2018年 wangxu的air. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibTool : NSObject
++(void)test;
+@end
---
+++ StaticLibTool.m
@@ -0,0 +1,15 @@
+//
+//  StaticLibTool.m
+//  StaticLibTool
+//
+//  Created by wangxu的air on 2018/12/10.
+//  Copyright © 2018年 wangxu的air. All rights reserved.
+//
+
+#import "StaticLibTool.h"
+
+@implementation StaticLibTool
++(void)test{
+    NSLog(@"测试");
+}
+@end
